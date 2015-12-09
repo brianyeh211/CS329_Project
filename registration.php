22673006ea262b5f8bf0ffd6268cf51663202731
ericnam <ericnam86@utexas.edu>
2015-12-09 17:04:49 -0600

    added registration.php

---
+++ registration.php
@@ -0,0 +1,32 @@
+<?php
+	extract($_POST);
+	$username = $_POST["username"];
+	$password = $_POST["password"];
+	$email = $_POST['email'];
+	$telephone = $_POST['telephone'];
+
+	//$password = crypt($password);
+
+	$combo = $username.":".$password.":".$telephone.":".$email;
+	
+	if ($fh = fopen('passwd.txt', 'r')){
+		while (($line = fgets($fh)) !== false) {
+			$line = trim($line, "\n");
+			$array = explode(":", $line);
+			$check = $array[0];
+			if ($check==$username) {
+				header('Location: fail.html');
+				exit;
+			}
+		}
+	}
+
+	fclose($fh);
+
+	if ($fh = fopen('passwd.txt', 'a')){
+		fwrite($fh, $combo.PHP_EOL);
+		$_SESSION['username'] = $username;
+		setcookie ('username', $combo, time()+120, '/', 'cs.utexas.edu', '0');
+		header('Location: Home.html');
+	}
+?>
